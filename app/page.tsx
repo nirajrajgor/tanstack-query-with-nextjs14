8415a9bc5f6b377b3ebb33a134f7d08f5bbddc4e
niraj <nirajr1992@gmail.com>
2024-11-04 23:45:08 -0800

    Add links to new pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -52,9 +53,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
         >
           <Image
             aria-hidden
@@ -63,13 +62,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Home
         </a>
-        <a
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/posts"
         >
           <Image
             aria-hidden
@@ -78,13 +75,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
+          Posts
+        </Link>
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/comments"
         >
           <Image
             aria-hidden
@@ -93,8 +88,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
-        </a>
+          Comments →
+        </Link>
       </footer>
     </div>
   );
