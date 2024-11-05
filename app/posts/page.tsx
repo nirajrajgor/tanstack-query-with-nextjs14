4997faa9b8e8f2ff3d71c30b28100bf888840a32
niraj <nirajr1992@gmail.com>
2024-11-05 15:52:25 -0800

    Create /post server component, fetch api data and pass as initialData to client component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,27 +1,29 @@
+import PostsClientPage from "./PostsClient";
+
 interface Post {
   id: number;
   title: string;
 }
 
-export default async function PostsPage() {
-  const posts = await fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json());
-
-    console.log('Post page called!!')
-
-  return (
-    <div className="p-4">
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post: Post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+async function fetchPost() {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
   );
 }
 
+export default async function PostsPage() {
+  // TODO: handle error if fetch failed.
+  const posts = await fetchPost();
+  return <PostsClientPage initialData={posts} />;
 
-
-
-
+  // return (
+  //   <div className="p-4">
+  //     <h1>Posts</h1>
+  //     <ul>
+  //       {posts.map((post: Post) => (
+  //         <li key={post.id}>{post.title}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+}
