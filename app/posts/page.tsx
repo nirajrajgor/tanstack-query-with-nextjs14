aad280798f44dd6c2a50c480e739508ed8a946d7
niraj <nirajr1992@gmail.com>
2024-11-07 00:01:37 -0800

    code cleanup

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -15,15 +15,4 @@ export default async function PostsPage() {
   // TODO: handle error if fetch failed.
   const posts = await fetchPost();
   return <PostsClientPage initialData={posts} />;
-
-  // return (
-  //   <div className="p-4">
-  //     <h1>Posts</h1>
-  //     <ul>
-  //       {posts.map((post: Post) => (
-  //         <li key={post.id}>{post.title}</li>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
 }
